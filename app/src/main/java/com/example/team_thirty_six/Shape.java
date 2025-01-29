d3186987aa397a1612adf7d197e04b94eef74d79
GalacticRe <dwlow@ualberta.ca>
2025-01-29 14:30:39 -0700

    Give shape a color -Dalton

--- app/src/main/java/com/example/team_thirty_six/Shape.java
+++ app/src/main/java/com/example/team_thirty_six/Shape.java
@@ -3,6 +3,7 @@ package com.example.team_thirty_six;
 public abstract class Shape {
     protected int x;
     protected int y;
+    protected String color = "Burgundy";
 
     public Shape(int x, int y) {
         this.x = x;
