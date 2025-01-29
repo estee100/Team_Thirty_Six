76b2512867f846bd2bd74023b4e6fab06914da58
estee100 <sktang@ualberta.ca>
2025-01-28 21:28:39 -0700

    Created Shape class

---
+++ app/src/main/java/com/example/team_thirty_six/Shape.java
@@ -0,0 +1,13 @@
+package com.example.team_thirty_six;
+
+public abstract class Shape {
+    protected int x;
+    protected int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
