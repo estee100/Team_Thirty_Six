db20c75a5f3f24f82167639b89154602e0c3e8be
aomotosh <aomotosh@ualberta.ca>
2025-01-31 09:19:51 -0700

    added colour

--- app/src/main/java/com/example/team_thirty_six/Shape.java
+++ app/src/main/java/com/example/team_thirty_six/Shape.java
@@ -3,8 +3,7 @@ package com.example.team_thirty_six;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "Deep Blue";
-
+    protected String color = "orange";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
