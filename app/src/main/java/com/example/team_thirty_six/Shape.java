08cebd2952879680c7702037e512f9ac9848650c
GalacticRe <dwlow@ualberta.ca>
2025-01-30 20:25:02 -0700

    Third attempt restart for merge conflict

--- app/src/main/java/com/example/team_thirty_six/Shape.java
+++ app/src/main/java/com/example/team_thirty_six/Shape.java
@@ -3,7 +3,6 @@ package com.example.team_thirty_six;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
