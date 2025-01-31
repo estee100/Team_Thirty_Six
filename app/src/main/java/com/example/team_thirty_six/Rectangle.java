dff1079575d531bf1f167c73151e1f66238024be
estee100 <sktang@ualberta.ca>
2025-01-30 22:27:16 -0700

    updated rectangle.java and added color to shape class

--- app/src/main/java/com/example/team_thirty_six/Rectangle.java
+++ app/src/main/java/com/example/team_thirty_six/Rectangle.java
@@ -6,6 +6,11 @@ public class Rectangle extends Shape {
     private double height;
     private double width;
 
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
     public double getHeight() {
         return height;
     }
