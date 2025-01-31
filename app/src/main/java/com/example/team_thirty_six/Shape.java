a6eff83f69decbfdf98f8c808d05fcf61839b225
subomiualberta <ooluyadi@ualberta.ca>
2025-01-30 22:50:39 -0700

    Added my color

--- app/src/main/java/com/example/team_thirty_six/Shape.java
+++ app/src/main/java/com/example/team_thirty_six/Shape.java
@@ -3,7 +3,7 @@ package com.example.team_thirty_six;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "orange";
+    protected String color = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
