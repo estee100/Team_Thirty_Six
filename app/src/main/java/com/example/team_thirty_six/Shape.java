62c792ca056bdf72467776e31714109a762ba7fb
Lawrence Omotosho <113223801+aomotosh@users.noreply.github.com>
2025-01-30 21:59:04 -0700

    Update Shape.java

--- app/src/main/java/com/example/team_thirty_six/Shape.java
+++ app/src/main/java/com/example/team_thirty_six/Shape.java
@@ -4,6 +4,7 @@ public abstract class Shape {
     protected int x;
     protected int y;
     protected String color = "orange";
+    protected String color = "Deep Blue";
 
     public Shape(int x, int y) {
         this.x = x;
